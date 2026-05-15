f90ce1dc6b82d61048fd32b108e08d9a52a20a68
krish <krishpatel3300@gmail.com>
2026-05-16 00:03:12 +0530

    factorial code added

---
+++ array/radehy.java
@@ -0,0 +1,8 @@
+class radehy {
+    public static void main(String[] args) {
+        String s = "is2 sentence4 This1 a3"
+        System.out.println(s.split(" ") );
+        
+
+    }
+}
---
+++ kody/Primenumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Primenumber {
+
+    public static int fact(int n){
+
+        int fact =1;
+        if(n ==0)
+        return 1;
+
+        else{
+            for(int i=1; i<=n ; i++)
+            {
+                fact = fact * i;
+            }
+            return fact;
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fact(n));
+    }
+}
